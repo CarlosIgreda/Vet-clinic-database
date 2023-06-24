d49fa4643814cc61eb2c802e4ccdf889c0210880
CarlosIgreda <cjigredarivas@gmail.com>
2023-06-24 17:30:49 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -39,3 +39,25 @@ ALTER TABLE animals ADD COLUMN owner_id integer;
 
 ALTER TABLE animals ADD CONSTRAINT fk_owners
 FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+/*Project 4*/
+
+CREATE TABLE vets(
+id serial PRIMARY KEY,
+name varchar(100),
+age integer,
+date_of_graduation date
+);
+
+CREATE TABLE specializations (
+    species_id INT REFERENCES species(id),
+	vet_id INT REFERENCES vets(id),
+    PRIMARY KEY (species_id, vet_id)
+);
+
+CREATE TABLE visits (
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    visit_date DATE,
+    PRIMARY KEY (animal_id, vet_id, visit_date)
+);
